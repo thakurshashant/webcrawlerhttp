3b3a37033591024dee761cda094f8b3ef6f45d1c
deathnote <127240347+thakurshashant@users.noreply.github.com>
2023-12-14 11:07:17 +0530

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,8 +1,7 @@
 const { JSDOM } = require ('jsdom')
 
 async function crawlPage(baseURL, currentURL,pages){
-    console.log(`actively crawling: ${currentURL}`)
- 
+    
     // we want to make sure that we are currently in same domain and we arent in any different domain 
 
     const baseURLObj = new URL (baseURL) 
@@ -13,22 +12,40 @@ async function crawlPage(baseURL, currentURL,pages){
 
     // now we want to keep a record of what pages i have already explored
 
+    const normalizedCurrentURL = normalizeURL(currentURL)
+
+    if(pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+    console.log(`actively crawling: ${currentURL}`)
+ 
+
     try {
         const resp = await fetch (currentURL)
         if(resp.status > 399){
             console.log(`error in fetch with status code : ${resp.status} on page: ${currentURL}`)
-            return 
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
 
         if(!contentType.includes("text/html")){
             console.log(`non html response, content type: ${contentType},  on page : ${currentURL}`)
-            return
+            return pages
         }
 
-        
-        console.log( await resp.text())
+        const htmlBody= await resp.text()
+        const nextURLs = getURLsFromHTML(htmlBody , baseURL)
+
+        for(const nextURL of nextURLs){
+            pages = await crawlPage(baseURL , nextURL ,pages)
+        }
+
+        return pages
+
     }catch( err){
         console.log(`error in fetch : ${err.message},on page: ${currentURL}`)
 
