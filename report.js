10bcaffbb2e0cbaf5ec69e180274cdd096c15b6e
deathnote <127240347+thakurshashant@users.noreply.github.com>
2023-12-15 10:54:53 +0530

    reports tested

--- report.js
+++ report.js
@@ -1,16 +1,16 @@
 function printReport(pages){
-    console.log("=======")
+    console.log("===========")
     console.log("REPORT")
-    console.log("=======")
+    console.log("==========")
     const sortedPages = sortPages(pages)
     for (const sortedPage of sortedPages){
         const url = sortedPage[0]
         const hits =sortedPage[1]
         console.log(`Found ${hits} links to page: ${url} `)
     }
-    console.log("=======")
+    console.log("=========")
     console.log("END REPORT")
-    console.log("=======")
+    console.log("=========")
 
 }
 
